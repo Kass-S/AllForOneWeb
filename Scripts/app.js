985f9326c24db1360bd9cd08d99e6b03f81a9203
Kass-S <kassiespacek101@gmail.com>
2025-01-24 09:19:03 -0800

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,18 @@
+
+
+
+const TitleText = document.getElementById("TitleText");
+const PlayBtn = document.getElementById("PlayBtn");
+const BackBtnHome = document.getElementById("BackBtnHome");
+
+const SayHelloPathBtn = document.getElementById("SayHelloPathBtn");
+const Add2NumsPathBtn = document.getElementById("Add2NumsPathBtn");
+const AskingQuestionsPathBtn = document.getElementById("AskingQuestionsPathBtn");
+const OddEvenPathBtn = document.getElementById("OddEvenPathBtn");
+const MadlibPathBtn = document.getElementById("MadlibPathBtn");
+const ReverseItNumPathBtn = document.getElementById("ReverseItNumPathBtn");
+const ReverseItStringPathBtn = document.getElementById("ReverseItStringPathBtn");
+const GreaterLessPathBtn = document.getElementById("GreaterLessPathBtn");
+const Magic8BallPathBtn = document.getElementById("Magic8BallPathBtn");
+const RestaurantPickerPathBtn = document.getElementById("RestaurantPickerPathBtn");
+
