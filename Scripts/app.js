8b2fed5621a574930379b480000969ea6a191c4b
Kass-S <kassiespacek101@gmail.com>
2025-01-26 23:36:16 -0800

    added styling for home page and to back btns

--- Scripts/app.js
+++ Scripts/app.js
@@ -16,5 +16,18 @@ const RestaurantPickerPathBtn = document.getElementById("RestaurantPickerPathBtn
 
 
 PlayBtn.addEventListener('click', () => {
-    
+    BackBtnHome.className = "font-inder bg-amber-50 rounded-2xl border-amber-100 border-3 text-3xl m-5 p-2 px-4";
+    SayHelloPathBtn.className = "flex justify-center align-middle my-5 bg-amber-50 rounded-2xl border-amber-950 border-3 py-4 mx-6";
+    Add2NumsPathBtn.className = "flex justify-center align-middle my-5 bg-amber-50 rounded-2xl border-amber-950 border-3 py-4 mx-6";
+    AskingQuestionsPathBtn.className = "flex justify-center align-middle my-5 bg-amber-50 rounded-2xl border-amber-950 border-3 py-4 mx-6";
+    OddEvenPathBtn.className = "flex justify-center align-middle my-5 bg-amber-50 rounded-2xl border-amber-950 border-3 py-4 mx-6";
+    MadlibPathBtn.className = "flex justify-center align-middle my-5 bg-amber-50 rounded-2xl border-amber-950 border-3 py-4 mx-6";
+    ReverseItNumPathBtn.className = "flex justify-center align-middle my-5 bg-amber-50 rounded-2xl border-amber-950 border-3 py-4 mx-6";
+    ReverseItStringPathBtn.className = "flex justify-center align-middle my-5 bg-amber-50 rounded-2xl border-amber-950 border-3 py-4 mx-6";
+    GreaterLessPathBtn.className = "flex justify-center align-middle my-5 bg-amber-50 rounded-2xl border-amber-950 border-3 py-4 mx-6";
+    Magic8BallPathBtn.className = "flex justify-center align-middle my-5 bg-amber-50 rounded-2xl border-amber-950 border-3 py-4 mx-6";
+    RestaurantPickerPathBtn.className = "flex justify-center align-middle col-[2] my-5 bg-amber-50 rounded-2xl border-amber-950 border-3 py-4 mx-6";
+    TitleText.className = "hidden";
+    PlayBtn.className = "hidden";
+
 })
